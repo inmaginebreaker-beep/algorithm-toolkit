bc7f7ebdf763d89ce8433d23ec2e75eecdf732c8
Kuroki <inmaginebreaker@gmail.com>
2026-07-25 12:36:21 +0800

    feat: add configuration management

--- src/algorithm_toolkit/main.py
+++ src/algorithm_toolkit/main.py
@@ -1,15 +1,29 @@
 import logging
 
-from algorithm_toolkit.logging_config import setup_logging
+from algorithm_toolkit.config import (
+    APP_NAME,
+    DEBUG
+)
+
+from algorithm_toolkit.logging_config import (
+    setup_logging
+)
 
 
 logger = logging.getLogger(__name__)
 
 
-def main() -> None:
+def main():
+
     setup_logging()
 
-    logger.info("Algorithm Toolkit started")
+    logger.info(
+        f"{APP_NAME} started"
+    )
+
+    logger.info(
+        f"debug={DEBUG}"
+    )
 
 
 if __name__ == "__main__":
